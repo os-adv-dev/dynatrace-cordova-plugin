a27ff4951eeed2b396ad146b7e88b65df7b609ae
Luis Bouça <luismrbouca@gmail.com>
2021-03-07 16:32:30 +0000

    test

--- src/default.config.js
+++ src/default.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     cordova : {
         debug : false,
-        autoUpdate : true
+        autoUpdate : true,
+        cspURL: "https://xgl76747.live.dynatrace.com/bf"
     },
 
     js : {
-        url : "",
-        mode : 0
+        url : "https://xgl76747.live.dynatrace.com?Api-Token=",
     },
 
     android : {
@@ -16,9 +16,13 @@ module.exports = {
             configurations {
                 defaultConfig {
                     autoStart {
-                        applicationId 'Insert your ApplicationID from Config'
+                        applicationId '2a1ef700-ac4e-4478-8206-bc8293d0a366'
                         beaconUrl 'https://bf14292qza.bf.dynatrace.com/mbeacon'
                     }
+                    hybridWebView {
+                        enabled true
+                    }
+                    agentBehavior.startupLoadBalancing true
                 }
             }
         }
@@ -29,9 +33,13 @@ module.exports = {
         // Those configs are copied 1:1
         config : `
         <key>DTXApplicationID</key>
-        <string>Insert your ApplicationID from Config</string>
+        <string>2a1ef700-ac4e-4478-8206-bc8293d0a366</string>
         <key>DTXBeaconURL</key>
-        <string>Insert your ClusterURL from Config</string>
+        <string>https://bf14292qza.bf.dynatrace.com/mbeacon</string>
+        <key>DTXHybridApplication</key>
+        <string>true</string>
+        <key>DTXStartupLoadBalancing</key>
+        <true/>
         `
-    }  
+    }
 }
